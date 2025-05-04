86b30b2c568e7d02f1ee61337edb72589727f94d
Adil Imran <devadem1370@gmail.com>
2025-05-04 04:53:33 -0700

    complete the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Hero/>
       <ShowcaseSection/>
     </div>
