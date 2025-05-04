42accb17e7c38c1002842415f2bdc458a250e0a5
Adil Imran <devadem1370@gmail.com>
2025-05-04 05:03:29 -0700

    complete feature  cards section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 import Navbar from './components/Navbar'
 import LogoSection from './components/LogoSection'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <ShowcaseSection/>
       <LogoSection/>
+      <FeatureCards/>
     </div>
   )
 }
