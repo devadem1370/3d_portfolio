154ecbbe91f68f50a63b1e92b60d417bff2a8836
Adil Imran <devadem1370@gmail.com>
2025-05-03 21:58:25 -0700

    add animation to the hero text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,17 @@
 import Button from "../components/Button"
 import Heroexperience from "../components/HeroModels/Heroexperience"
 import { words } from "../constants"
-
+import { useGSAP } from "@gsap/react"
+import gsap from 'gsap'
 const Hero = () => {
+
+    useGSAP(() => {
+        gsap.fromTo(
+          ".hero-text h1",
+          { y: 50, opacity: 0 },
+          { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+        );
+      });
   return (
     <section id='#hero' className='ralative overflow-hidden'>
         <div className="absolute top-0 left-0 z-10">
